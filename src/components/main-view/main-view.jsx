bcb781da67e0c119f47fac59f7efc769d61f8d33
Niket Shukla <niketshukla@gmail.com>
2022-04-15 16:15:14 -0400

    Created login and registration view as well as implemented Hooks and proptypes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView} from '../register-view/register-view';
 
 class MainView extends React.Component {
   constructor() {
@@ -10,6 +12,7 @@ class MainView extends React.Component {
     this.state = {
       movies: [],
       selectedMovie: null,
+      user: null
     };
   }
 
@@ -31,13 +34,23 @@ class MainView extends React.Component {
     });
   }
 
-  render() {
-    const { movies, selectedMovie } = this.state;
+  onLoggedIn(user){
+    this.setState({
+      user
+    });
+  }
 
+  render() {
+    const { movies, selectedMovie, user } = this.state;
+
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+    // Before the movies have been loaded
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
+        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
